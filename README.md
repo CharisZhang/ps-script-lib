9fe2ebdbf3540fec763d7877d88a67b04d0499f9
张超 <tochariszhang@sina.com>
2021-07-22 14:38:46 +0800

    img-pdf

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ps-script-lib
 Script library for Photoshop
+
+
+- img-pdf 
+    - 每两张图片合成pdf
---
+++ img-pdf.js
@@ -0,0 +1,35 @@
+var inputFolder = new Folder("D:/Data/My Desktop/1")
+var inputFiles = inputFolder.getFiles("*.jpg")
+
+
+inputFiles = inputFiles.sort()
+
+var names = []
+for (var i = 0; i < inputFiles.length; i++) {
+  names.push(inputFiles[i])
+  if (names.length == 2) {
+    names = names.sort(sortBy)
+    var endFileName = 'D:/Data/My Desktop/2/' + names[0].name.split('.')[0] + '.pdf'
+    saveToPdf(names, endFileName)
+  }
+  if (i % 2 == 1) {
+    names = []
+  }
+}
+
+
+function sortBy(a, b) {
+  return a.name.length - b.name.length
+}
+
+function saveToPdf(inputFiles, fileName) {
+  var outputFile = File(fileName)
+  var options = new PresentationOptions
+  options.presentation = true
+  options.view = true
+  options.autoAdvance = true
+  options.interval = 5
+  options.loop = true
+  options.transition = TransitionType.RANDOM
+  makePDFPresentation(inputFiles, outputFile, options)
+}
